a38d9ef88ecfef9f4cfd5dee3962c21edecd1eba
Warren Strange <warren.strange@gmail.com>
2020-12-08 09:08:50 -0700

    cp kustomize to bin (#22)
    Co-authored-by: Warren Strange <warren.strange@forgerock.com>

--- hack/install-kustomize.sh
+++ hack/install-kustomize.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
-# Make kustomize available on the gopath/bin - where the make process can find it
-cp kustomize $(go env GOPATH)/bin
-
+# Make kustomize available on the path
+cp kustomize /bin && chmod a+rx /bin/kustomize
