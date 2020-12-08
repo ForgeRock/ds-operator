0d965e4551e2ebfd248d6079838e9b4280ada1fd
Warren Strange <warren.strange@forgerock.com>
2020-12-08 15:25:18 +0000

    add kustomize to gopath

--- hack/install-kustomize.sh
+++ hack/install-kustomize.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+# Make kustomize available on the gopath/bin - where the make process can find it
+cp kustomize $(go env GOPATH)/bin
 
