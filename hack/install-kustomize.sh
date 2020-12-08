9628f203b349c97140e29d38d6eb3c489a429f8a
Warren Strange <warren.strange@gmail.com>
2020-12-08 09:23:11 -0700

    leave git clean (#23)
    Co-authored-by: Warren Strange <warren.strange@forgerock.com>

--- hack/install-kustomize.sh
+++ hack/install-kustomize.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
 # Make kustomize available on the path
-cp kustomize /bin && chmod a+rx /bin/kustomize
+mv kustomize /bin && chmod a+rx /bin/kustomize
