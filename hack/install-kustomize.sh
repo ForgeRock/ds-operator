d28e1137d77a2c9a3e7a155b558e2aa6a407b787
Max Resnick <max.resnick@forgerock.com>
2020-12-07 17:26:32 -0800

    use kustomize install script

--- hack/install-kustomize.sh
+++ hack/install-kustomize.sh
@@ -1,33 +1,3 @@
 #!/usr/bin/env bash
-if type -P kustomize &> /dev/null;
-then
-	echo "kustomize installed, not installing";
-	exit;
-fi
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
 
-case "${OSTYPE}" in
-    "darwin"*) os="darwin";;
-    "linux"*) os="linux";;
-esac
-
-latest=$(curl -s -w "%{redirect_url}" https://github.com/kubernetes-sigs/kustomize/releases/latest -o /dev/null | awk -F "/v" '{ printf $NF }')
-if ! curl -o /tmp/kustomize.tar.gz -L "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${latest}/kustomize_v${latest}_${os}_amd64.tar.gz";
-then
-    echo "failed to download kustomize"
-    exit 1;
-fi
-if ! tar vxf /tmp/kustomize.tar.gz -C /tmp/;
-then
-    echo "failed to extract kustomize"
-    exit 1;
-fi
-if ! install /tmp/kustomize /go/bin/kustomize
-then
-    echo "failed to install to bin"
-    exit 1;
-fi
-if ! rm -rf /tmp/{kustomize,kustomize.tar.gz};
-then
-    echo "failed to cleanup"
-    exit 1;
-fi
