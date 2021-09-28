544bdaa512d9f9ee84f3ff5c96b9aefe9f5fe432
Warren Strange <warren.strange@forgerock.com>
2021-09-28 17:09:43 -0600

    fix: password update time should be in annotation (#59)
    * fix: password update time should be in annotation
    
    Use an annotation to record the last password update timestamp
    
    * removing readme that is obsolete
    updating test/ds-snapshots.yaml to remove duplicates and set the period to ten minutes so we dont trip quota

--- controllers/passwords.go
+++ controllers/passwords.go
@@ -6,6 +6,7 @@ package controllers
 
 import (
 	"context"
+	"strconv"
 	"time"
 
 	directoryv1alpha1 "github.com/ForgeRock/ds-operator/api/v1alpha1"
@@ -15,27 +16,45 @@ import (
 	k8slog "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
-// PasswordCheckTimeSeconds is the number of seconds between password checks. The first time through will trigger an immediate check.
-// This is to avoid overloading the directory.
+// PasswordCheckTimeSeconds is the number of seconds between password checks.
+// This is to avoid overloading the directory with frequent password checks/changes
 const PasswordCheckTimeSeconds = 300
+
+// annotation we add to the CR to record when we last checked the passwords to see if they are correct in the diretory
+const LastPasswordCheckAnnotation = "directory.forgerock.io/last-password-check"
 
 /// Manages ldap service account passwords in Spec.Passwords
 func (r *DirectoryServiceReconciler) updatePasswords(ctx context.Context, ds *directoryv1alpha1.DirectoryService, ldap *ldap.DSConnection) error {
 	log := k8slog.FromContext(ctx)
 
 	now := time.Now().Unix()
-	elapsed := now - ds.Status.ServiceAccountPasswordsUpdatedTime
+	annotations := ds.ObjectMeta.GetAnnotations()
+	lastTime := annotations[LastPasswordCheckAnnotation]
 
-	// if it is to soon, skip the check so we are nice to the directory
-	if elapsed < PasswordCheckTimeSeconds {
-		return nil
+	if lastTime != "" {
+		lastTimeInt, err := strconv.ParseInt(lastTime, 10, 64)
+		if err != nil {
+			log.Error(err, "Failed to parse last password check time")
+			// set to 0 so we force a pw change, and this will reset the annotation
+			lastTimeInt = 0
+		}
+		elapsed := now - lastTimeInt
+
+		//fmt.Printf("DEBUG - elapsed time since password check %d\n", elapsed)
+
+		// if it is too soon, skip the check so we are nice to the directory
+		if elapsed < PasswordCheckTimeSeconds {
+			return nil
+		}
 	}
-	// for each password
+
+	// for each password...
 	for key, account := range ds.Spec.Passwords {
 		// skip any internal accounts as they are managed on boot by the ds image
 		if key == "uid=admin" || key == "uid=monitor" {
 			continue
 		}
+
 		log.V(5).Info("Updating service account password", "dn", key, "secretName", account.SecretName, "key", account.Key)
 		// get the secret containing the password
 		pw, err := r.lookupSecret(ctx, ds.Namespace, ds.SecretNameForDN(key), account.Key)
@@ -43,9 +62,9 @@ func (r *DirectoryServiceReconciler) updatePasswords(ctx context.Context, ds *di
 			log.Error(err, "Can't find secret containing the password", "account", key, "secretName", account.SecretName)
 			return err
 		}
-		// First check to see if we even need to change the password. It may be fine
+		// First check to see if we even need to change the password. It may be fine in which case we can go to the next one.
 		if ldap.BindPassword(key, pw) == nil {
-			log.V(5).Info("Current password for DN is OK", "dn", key)
+			log.V(5).Info("Current password for DN does not need to be changed", "dn", key)
 			continue
 		}
 		// Bind above failed, so lets try to change it:
@@ -57,6 +76,8 @@ func (r *DirectoryServiceReconciler) updatePasswords(ctx context.Context, ds *di
 	}
 	// if we get to here we have updated all passwords OK
 	ds.Status.ServiceAccountPasswordsUpdatedTime = now
+	annotations[LastPasswordCheckAnnotation] = strconv.FormatInt(now, 10)
+	ds.ObjectMeta.SetAnnotations(annotations)
 
 	return nil
 }
