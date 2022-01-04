5b5e12dd7942b08315d424264eec8d010cfa5720
Warren Strange <warren.strange@forgerock.com>
2022-01-04 21:35:42 +0000

    chore: Add secret migration script, doc fixes
    adds a script to convert existing secret agent secrets to the same format used by cert-manager.

---
+++ hack/cp-sa-secret.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+# Sample script to migrate secret agent generated ds secrets to the tls format used by cert-manager
+# This can be used as a transition mechanism to reuse a saved secret agent secret.
+# After migration, the secret agent secrets should be deprecated in favour of cert-manager certs.
+
+D=/tmp/sa-secrets
+
+rm -fr $D
+mkdir -p $D
+
+# Get the DS and platform ca secrets
+kubectl get secret ds -o json >$D/ds-secret.json
+kubectl get secret platform-ca -o json >$D/ca.json
+
+# extract the base64 PEM certs
+jq <$D/ca.json -r  .data.'"ca.pem"' | base64 -d > $D/ca.pem
+jq <$D/ds-secret.json -r  .data.'"ssl-key-pair-private.pem"' | base64 -d > $D/ssl-private.pem
+jq <$D/ds-secret.json -r  .data.'"ssl-key-pair.pem"' | base64 -d > $D/ssl-keypair.pem
+
+
+
+cat >$D/ssl-tls.yaml <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ds-ssl-keypair
+type: kubernetes.io/tls
+data:
+  ca.crt: $(cat $D/ca.pem | base64 -w0)
+  tls.crt: $(cat $D/ssl-keypair.pem | base64 -w0)
+  tls.key: $(cat $D/ssl-private.pem | base64 -w0)
+EOF
+
+echo "Generated tls.yaml in $D."
+echo "apply with kubectl -f $D/ssl-tls.yaml"
+cat $D/ssl-tls.yaml
+
+# Uncomment if you want to do this automatically
+# kubectl apply -f $D/ssl-tls.yaml
+
+# Now create the ds master keypair
+jq <$D/ds-secret.json -r  .data.'"master-key-pair-private.pem"' | base64 -d > $D/master-private.pem
+jq <$D/ds-secret.json -r  .data.'"master-key-pair.pem"' | base64 -d > $D/master-keypair.pem
+
+cat >$D/master-tls.yaml <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ds-master-keypair
+type: kubernetes.io/tls
+data:
+  tls.crt: $(cat $D/master-keypair.pem | base64 -w0)
+  tls.key: $(cat $D/master-private.pem | base64 -w0)
+EOF
+
+echo ""
+echo "Generated $D/master-tls.yaml"
+echo "apply with kubectl -f $D/master-tls.yaml"
+cat $D/master-tls.yaml
+
+# Uncomment if you want to do this automatically
+# kubectl apply -f $D/master-tls.yaml
